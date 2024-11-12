6ad435f06393691a247c0a28931b0ceb1573aa77
Michelangelo Calori <70917863+mikizdt@users.noreply.github.com>
2024-11-11 22:39:18 -0500

    cleanup

--- Bookstore/src/Components/Home.jsx
+++ Bookstore/src/Components/Home.jsx
@@ -2,8 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
 
 export default function Home() {
-  const [books, setBooks] = useEffect([]);
-
   return (
     <>
       <h1
@@ -16,23 +14,11 @@ export default function Home() {
         BookSearch
       </h1>
 
-      <nav className="bg-primary fs-5 justify-content-center">
-        <ul className="d-flex list-unstyled justify-content-around text-white">
-          <li>Categories</li>
-          <li>Author&apos;s</li>
-          <li>Date</li>
-          <li>Watch List</li>
-          <li>Suggest random book</li>
-        </ul>
-      </nav>
-      <div className="p-2 fs-2  justify-content-center d-flex m-3  text-primary">
-        👀 What books are you looking for?
-      </div>
       <div className="container  d-flex justify-content-center">
         <div className="search d-flex w-100 justify-content-center align-items-center">
           <input
             className="m-2 w-50 p-1 rounded-lg form-control"
-            placeholder="Search..."
+            placeholder="Search For Book's..."
             id="searchInput"
             type="text"
           />
